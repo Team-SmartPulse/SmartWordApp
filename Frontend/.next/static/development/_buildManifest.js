24335913cb4c16415e3e30ce7b881a2a167465e8
Adjushi Akpevwe Favour <122548401+Iceprincess2@users.noreply.github.com>
2023-05-19 09:17:47 +0100

    Delete Frontend/.next directory

--- Frontend/.next/static/development/_buildManifest.js
+++
@@ -1,9 +0,0 @@
-<<<<<<< HEAD
-self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static\u002Fchunks\u002Fpages\u002Findex.js"],"/Game":["static\u002Fchunks\u002Fpages\u002FGame.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],sortedPages:["\u002F","\u002FGame","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
-=======
-<<<<<<< HEAD
-self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/Game/level":["static\u002Fchunks\u002Fpages\u002FGame\u002Flevel.js"],"/Game/stages":["static\u002Fchunks\u002Fpages\u002FGame\u002Fstages.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],sortedPages:["\u002FGame\u002Flevel","\u002FGame\u002Fstages","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
-=======
-self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/marketplace":["static\u002Fchunks\u002Fpages\u002Fmarketplace.js"],sortedPages:["\u002F_app","\u002F_error","\u002Fmarketplace"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
->>>>>>> eba42f5372d0f42d828b21b72b30966ef00fabd3
->>>>>>> 5095bb57607724c0afdd87d27d7e9c1572237ffe
